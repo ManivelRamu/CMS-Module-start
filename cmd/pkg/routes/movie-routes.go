74ebb79d48a096fc83656d70a0be0e2f476b84cb
Levin <levincruze12@gmail.com>
2022-08-03 11:34:50 +0530

    Bug fix

--- cmd/pkg/routes/movie-routes.go
+++ cmd/pkg/routes/movie-routes.go
@@ -7,32 +7,32 @@ import (
 )
 
 var RegisterMovieRoutes = func(router *mux.Router) {
-	router.HandleFunc("/movies/", middleware.VerifyLogin(controllers.CreateMovie)).Methods("POST")
-	router.HandleFunc("/movies/", controllers.GetMovie).Methods("GET")
+	router.HandleFunc("/movies", middleware.VerifyLogin(controllers.CreateMovie)).Methods("POST")
+	router.HandleFunc("/movies", middleware.VerifyLogin(controllers.GetMovie)).Methods("GET")
 	//router.HandleFunc("/movies/pagination", controllers.GetAllUsers).Methods("GET")
 	router.HandleFunc("/movies/search/{attribute}/{value}", controllers.SearchMovie).Methods("GET")
-	router.HandleFunc("/movies/{movieId}", controllers.GetMovieById).Methods("GET")
+	router.HandleFunc("/movies/{movieId}", middleware.VerifyLogin(controllers.GetMovieById)).Methods("GET")
 	router.HandleFunc("/movies/{movieId}", middleware.VerifyLogin(controllers.UpdateMovie)).Methods("PUT")
 	router.HandleFunc("/movies/{movieId}", middleware.VerifyLogin(controllers.DeleteMovie)).Methods("DELETE")
 
 	//show routes
-	router.HandleFunc("/show/", middleware.VerifyLogin(controllers.Createshow)).Methods("POST")
-	router.HandleFunc("/show/", controllers.Getshow).Methods("GET")
-	router.HandleFunc("/show/{showId}", controllers.GetshowById).Methods("GET")
+	router.HandleFunc("/show", middleware.VerifyLogin(controllers.Createshow)).Methods("POST")
+	router.HandleFunc("/show", middleware.VerifyLogin(controllers.Getshow)).Methods("GET")
+	router.HandleFunc("/show/{showId}", middleware.VerifyLogin(controllers.GetshowById)).Methods("GET")
 	router.HandleFunc("/show/{showId}", middleware.VerifyLogin(controllers.UpdateShow)).Methods("PUT")
 	router.HandleFunc("/show/{showId}", middleware.VerifyLogin(controllers.Deleteshow)).Methods("DELETE")
 
 	//season routes
-	router.HandleFunc("/season/", middleware.VerifyLogin(controllers.Createseason)).Methods("POST")
-	router.HandleFunc("/season/", controllers.Getseason).Methods("GET")
-	router.HandleFunc("/season/{seasonId}", controllers.GetseasonById).Methods("GET")
+	router.HandleFunc("/season", middleware.VerifyLogin(controllers.Createseason)).Methods("POST")
+	router.HandleFunc("/season", middleware.VerifyLogin(controllers.Getseason)).Methods("GET")
+	router.HandleFunc("/season/{seasonId}", middleware.VerifyLogin(controllers.GetseasonById)).Methods("GET")
 	router.HandleFunc("/season/{seasonId}", middleware.VerifyLogin(controllers.Updateseason)).Methods("PUT")
 	router.HandleFunc("/season/{seasonId}", middleware.VerifyLogin(controllers.Deleteseason)).Methods("DELETE")
 
 	//EpisodeRoutes
-	router.HandleFunc("/episode/", middleware.VerifyLogin(controllers.Createepisode)).Methods("POST")
-	router.HandleFunc("/episode/", controllers.Getepisode).Methods("GET")
-	router.HandleFunc("/episode/{episodeId}", controllers.GetepisodeById).Methods("GET")
+	router.HandleFunc("/episode", middleware.VerifyLogin(controllers.Createepisode)).Methods("POST")
+	router.HandleFunc("/episode", middleware.VerifyLogin(controllers.Getepisode)).Methods("GET")
+	router.HandleFunc("/episode/{episodeId}", middleware.VerifyLogin(controllers.GetepisodeById)).Methods("GET")
 	router.HandleFunc("/episode/{episodeId}", middleware.VerifyLogin(controllers.Updateepisode)).Methods("PUT")
 	router.HandleFunc("/episode/{episodeId}", middleware.VerifyLogin(controllers.Deleteepisode)).Methods("DELETE")
 
